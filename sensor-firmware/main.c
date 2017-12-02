04c2109a927934f2acaf177a4345b1b64410a0c0
Michael Spieler <michael.spieler@epfl.ch>
2017-12-02 01:39:22 +0100

    Working RGB sensor test

--- sensor-firmware/main.c
+++ sensor-firmware/main.c
@@ -51,6 +51,38 @@ static const I2CConfig i2cconfig = {
 #define TCS3472_REG_BDATAL      (0x1A)
 #define TCS3472_REG_BDATAH      (0x1B)
 
+void TCS3472_write_reg(uint8_t reg, uint8_t val)
+{
+    uint8_t txbuf[2];
+    txbuf[0] = reg | TCS3472_CMD_AUTO_INC;
+    txbuf[1] = val;
+    i2cMasterTransmitTimeout(&I2CD1, TCS3472_I2C_ADDR, txbuf, 2, NULL, 0, TIME_INFINITE);
+}
+
+uint8_t TCS3472_read_reg(uint8_t reg)
+{
+    uint8_t txbuf[1] = {reg | TCS3472_CMD_AUTO_INC};
+    uint8_t rxbuf[1];
+    i2cMasterTransmitTimeout(&I2CD1, TCS3472_I2C_ADDR, txbuf, 1, rxbuf, 1, TIME_INFINITE);
+    return rxbuf[0];
+}
+
+void TCS3472_read_color(uint16_t crgb[4])
+{
+    while ((TCS3472_read_reg(TCS3472_REG_STATUS) & 1) == 0) {
+        chThdSleepMilliseconds(1);
+    }
+
+    uint8_t txbuf[1] = {TCS3472_REG_CDATAL | TCS3472_CMD_AUTO_INC};
+    uint8_t rxbuf[8];
+
+    i2cMasterTransmitTimeout(&I2CD1, TCS3472_I2C_ADDR, txbuf, 1, rxbuf, 8, TIME_INFINITE);
+
+    crgb[0] = rxbuf[0] | ((uint16_t)rxbuf[1]<<8);
+    crgb[1] = rxbuf[2] | ((uint16_t)rxbuf[3]<<8);
+    crgb[2] = rxbuf[4] | ((uint16_t)rxbuf[5]<<8);
+    crgb[3] = rxbuf[6] | ((uint16_t)rxbuf[7]<<8);
+}
 
 int main(void)
 {
@@ -69,17 +101,20 @@ int main(void)
 
     i2cStart(&I2CD1, &i2cconfig);
 
-    uint8_t txbuf[1] = {TCS3472_REG_ID | TCS3472_CMD_AUTO_INC};
-    uint8_t rxbuf[1] = {0};
-    msg_t m = i2cMasterTransmitTimeout(&I2CD1, TCS3472_I2C_ADDR, txbuf, 1, rxbuf, 1, TIME_INFINITE);
+    uint8_t id = TCS3472_read_reg(TCS3472_REG_ID);
 
-    if (m != MSG_OK) {
-        chprintf(stdout, "i2c error\n");
-    }
+    chprintf(stdout, "TCS3472_REG_ID = 0x%2x\n", id);
 
-    chprintf(stdout, "TCS3472_REG_ID = 0x%2x\n", rxbuf[0]);
+    TCS3472_write_reg(TCS3472_REG_ENABLE, 0b00001011); // PON, AEN, WEN -> enable continuous RGBC conversion, with wait
+    TCS3472_write_reg(TCS3472_REG_ATIME, 0xF6); // 24 ms integration time
+    TCS3472_write_reg(TCS3472_REG_WTIME, 0xAB); // 204 ms wait time
+    TCS3472_write_reg(TCS3472_REG_CONFIG, 0x00); // normal wait time
+    TCS3472_write_reg(TCS3472_REG_CONTROL, 0b00000010); // 16x gain
 
     while (true) {
-        chThdSleepMilliseconds(500);
+        chThdSleepMilliseconds(100);
+        uint16_t crgb[4];
+        TCS3472_read_color(crgb);
+        chprintf(stdout, "CRGB %u %u %u %u\n", crgb[0], crgb[1], crgb[2], crgb[3]);
     }
 }
